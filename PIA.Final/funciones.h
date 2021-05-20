12439b9d484137faaf61dd437a8a14bdd9adba4f
Diegogochi <66629983+Diegogochi@users.noreply.github.com>
2021-05-20 00:11:38 -0500

    Add files via upload

---
+++ PIA.Final/funciones.h
@@ -0,0 +1,18 @@
+void cine_cliente();
+void cine_trabajo();
+void cartelera();
+void dulceria();
+void matrices();
+void ticket();
+int obtainSits(int,int,char);
+void beginSITS(char);
+void printSeparateHeaders();
+void printSeparateRows();
+void printHeader();
+char enteroACaracter(int);
+void printRoom(char, int);
+int openCase(char, int, char);
+void finalroom();
+int asientos();
+
+
