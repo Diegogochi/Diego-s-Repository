44b173cecfb94ff4c56b1cf2bf905c7704790474
Diegogochi <66629983+Diegogochi@users.noreply.github.com>
2021-06-15 06:22:25 -0500

    Delete Chapter 1 directory

--- Chapter 1/Exercise 1.6.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main(){
-	
-	printf("Enter a character: \n");
-	printf("The expression \"getchar() != EOF\" is %d. \n", getchar()!= EOF); //The "\" is to make an expression printable, i think
-	
-	
-}
